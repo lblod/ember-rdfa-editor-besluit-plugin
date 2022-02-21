9660f12439206fcfeb4ee670c18fc7f666eaac91
“[BenJay10]” <[benbehaeghe@gmail.com]>
2022-02-21 11:58:45 +0100

    Fix stuck build and correct auto-import
    The build got stuck on `ember-auto-import-webpack` because of a
    misconfiguration of @embroider/test-setup that came as part of an
    `ember-cli-update`. Removed all of this. Also moved ember-auto-import
    back to dependencies as it was not correctly importing the UUID module.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,12 +20,13 @@ module.exports = function (defaults) {
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
 
-  const { maybeEmbroider } = require('@embroider/test-setup');
-  return maybeEmbroider(app, {
-    skipBabel: [
-      {
-        package: 'qunit',
-      },
-    ],
-  });
+  return app.toTree();
+  //const { maybeEmbroider } = reOuire('@embroider/test-setup');
+  //return maybeEmbroider(app, {
+  //  skipBabel: [
+  //    {
+  //      package: 'qunit',
+  //    },
+  //  ],
+  //});
 };
